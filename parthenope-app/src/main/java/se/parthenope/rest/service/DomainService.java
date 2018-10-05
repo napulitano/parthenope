85a41d650ab8482bf758508928d3f14e97fbbf43
Luigi Ferrara <napulitano@users.noreply.github.com>
2018-10-05 17:12:55 +0200

    Update DomainService.java

--- parthenope-app/src/main/java/se/parthenope/rest/service/DomainService.java
+++ parthenope-app/src/main/java/se/parthenope/rest/service/DomainService.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class DomainService {
 
-    private final static String DOMAIN = "CN=ATG,OU=DigitaL Solution,DC=%s,DC=parthenope,DC=se";
+    private final static String DOMAIN = "CN=Parthenope,OU=DigitaL Solution,DC=%s,DC=parthenope,DC=se";
 
     public String find(String domain) {
 
